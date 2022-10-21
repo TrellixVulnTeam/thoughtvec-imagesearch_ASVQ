f847caa8ee336d1e386c2c37c2a331469a84e114
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-21 02:46:50 +0000

    Adding tarfile member sanitization to extractall()

--- SentenceEncoderUniSkip.py
+++ SentenceEncoderUniSkip.py
@@ -46,7 +46,26 @@ class SentenceEncoderUniSkip():
         if not os.path.isdir(unzipped_path):
             print("extracting skip_thoughts_uni parameters from zip file...")
             with tarfile.open(model_params_fp) as tar:
-                tar.extractall(path=self.model_params_dir)
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
+                    
+                
+                safe_extract(tar, path=self.model_params_dir)
                 tar.close()
 
     def materialize(self, encoder_mgr=None):
